f13838e5880b17399932e9b83b8577b1e24567bb
Gyrd Brændeland <gyrdbr@gmail.com>
2026-02-05 09:44:47 +0100

    smaating

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 import *  as sqlite3  from 'sqlite3';
 import { printQueryResults } from './utils';
 
+
 // const { printQueryResults } = require('./utils');
 // const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db.sqlite');
@@ -17,9 +18,11 @@ db.all('SELECT * FROM TemperatureData ORDER BY year', (error: string | null, row
   if (error) {
     throw error;
   }
-  printQueryResults(rows);
+  console.log("TemperatureData rows:", rows.length);
+  //printQueryResults(rows);
 });
 
 db.all("SELECT * FROM Dog WHERE breed='Corgi'", (error: string | null, rows: Array<any>) => {
-  printQueryResults(rows);
+    console.log("Corgi rows:", rows.length);
+  // printQueryResults(rows);
 });
