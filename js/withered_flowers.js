85aed676554e6fe5dfe4ac585da734c9bf942cec
Gyrd Brændeland <gyrdbr@gmail.com>
2026-04-20 15:49:57 +0200

    todo-fil withered_flowers.js

---
+++ js/withered_flowers.js
@@ -0,0 +1,11 @@
+// gsap.config({ trialWarn: false });
+
+/* visne blomster */
+
+// TODO: kopier gBlomstHeleVenstre og bruk den til å lage de andre blomstene, så slipper vi å tegne hver enkelt blomst for hånd
+// lag en knopp med utspring for topp-blomsten
+// animer knoppen til å vokse og blomstre, og deretter animere de andre blomstene til å blomstre etter hverandre
+
+const gBlomstHeleVenstre = document.querySelector('.blomst-hele-venstre');
+const gBlomstHeleMidten = document.querySelector('.blomst-hele-midten');
+const gBlomstHeleHøyre = document.querySelector('.blomst-hele-høyre');
