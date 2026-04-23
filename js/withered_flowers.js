73c14678e3f370f447f0b0491ddb5ab9f5f7ac41
Gyrd Brændeland <gyrdbr@gmail.com>
2026-04-23 10:28:10 +0200

    5 blader paa den oeverste  knoppen

--- js/withered_flowers.js
+++ js/withered_flowers.js
@@ -2,7 +2,8 @@
 
 /* visne blomster */
 
-// TODO: animer knoppen til å vokse og blomstre, og deretter animere de andre blomstene til å blomstre etter hverandre
+// TODO:  animer knoppen til å vokse og blomstre, og deretter animere de andre blomstene til å blomstre etter hverandre
+// foer mer figur-laging
 
 const gBlomstHeleVenstre = document.querySelector('.blomst-hele-venstre');
 const gBlomstHeleMidten = document.querySelector('.blomst-hele-midten');
