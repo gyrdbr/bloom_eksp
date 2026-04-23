92eb892e9f125dd190bbdee27562ac2dcbb1ebfd
Gyrd Brændeland <gyrdbr@gmail.com>
2026-04-23 08:38:27 +0200

    flere blader i blomsten

--- js/withered_flowers.js
+++ js/withered_flowers.js
@@ -2,8 +2,7 @@
 
 /* visne blomster */
 
-// TODO: kopier gBlomstHeleVenstre og bruk den til å lage de andre blomstene, så slipper vi å tegne hver enkelt blomst for hånd
-// animer knoppen til å vokse og blomstre, og deretter animere de andre blomstene til å blomstre etter hverandre
+// TODO: animer knoppen til å vokse og blomstre, og deretter animere de andre blomstene til å blomstre etter hverandre
 
 const gBlomstHeleVenstre = document.querySelector('.blomst-hele-venstre');
 const gBlomstHeleMidten = document.querySelector('.blomst-hele-midten');
