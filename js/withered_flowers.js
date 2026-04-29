f2e5b850f50d25d26b21977a9721f445ad6fc63f
Gyrd Brændeland <gyrdbr@gmail.com>
2026-04-29 16:45:27 +0200

    iris-blomst eksperimentering med morphSVG II

--- js/withered_flowers.js
+++ js/withered_flowers.js
@@ -35,12 +35,13 @@ habitFlowerTransform.animateHabitFlower();
 
 // TweenMax.to('#rect', 5, { rotation: "+=90", ease: Linear.easeNone, transformOrigin:"50% 50%" });
 
+/*
 let tl = gsap
   .timeline({
     defaults: { duration: 2, ease: "expo.inOut" },
-    repeat: -1
   })
   .to("#morph", { morphSVG: "#speech" })
+  */
 
 // GSDevTools.create({animation: tl, minimal: true});
 
