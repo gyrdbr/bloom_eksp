ab0ae4346f5b048caa5d18ec3b68922909aed3de
Gyrd Brændeland <gyrdbr@gmail.com>
2026-05-08 12:39:33 +0200

    endringer i iris_blomstV2FormatTest.svg

--- js/withered_flowers.js
+++ js/withered_flowers.js
@@ -66,7 +66,6 @@ let tn = gsap
 // animering av hovedstilk og blomster
 // animering i main funker naa. maa bare flytte den. saa endre stylinga paa siste steg
 
-/*
 let to = gsap
     .timeline({
         defaults: { duration: 2, ease: "expo.inOut" },
@@ -82,5 +81,5 @@ let to = gsap
         defaults: { delay: 1, duration: 2, ease: "expo.inOut" },
     })
     .to("#pUtspring2VenstreToppen", { morphSVG: "#pBlomstLeftMain-2-2-5" })
-*/
+
     
