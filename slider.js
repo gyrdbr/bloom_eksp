4577cbc2cd2767489a424c23108dc010edcccb4d
Gyrd Brændeland <gyrdbr@gmail.com>
2026-08-07 20:44:04 +0200

    la koden for hidden i slider

--- slider.js
+++ slider.js
@@ -28,3 +28,31 @@ prevBtn.addEventListener('click', () => {
   }
   updateSlider();
 });
+
+
+/* 
+// TODO: kjoer koden her
+
+      <a href="#until-found-box">Go to hidden content</a>
+
+      <div>I'm not hidden</div>
+      <div id="until-found-box" hidden="until-found">Hidden until found</div>
+      <div>I'm not hidden, either</div>
+      <button id="reset">Reset</button>
+
+   <script>
+      console.log(document.querySelector("#until-found-box"));
+          
+      const untilFound = document.querySelector("#until-found-box");
+      untilFound.addEventListener(
+         "beforematch",
+            () => (untilFound.textContent = "I've been revealed!"),
+         );
+
+      document.querySelector("#reset").addEventListener("click", () => {
+      document.location.hash = "";
+      document.location.reload();
+      });
+   </script>
+
+*/
