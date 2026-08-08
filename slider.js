b0d690f160512c6f50107f4ae32eec04434a9cc4
Gyrd Brændeland <gyrdbr@gmail.com>
2026-08-08 15:13:35 +0200

    den delen som ble fjerne brukes ikke lenger

--- slider.js
+++ slider.js
@@ -28,27 +28,7 @@ prevBtn.addEventListener('click', () => {
   }
   updateSlider();
 });
-    
-// const allUntilFounds = document.querySelectorAll(".hiddenNow");
-/*
-const untilFound = document.querySelector("#until-found-box");
-const allFounds = document.querySelectorAll(".hiddenNow");
 
-console.log("allFounds", allFounds);
-
-// Loop through the items to apply changes
-allFounds.forEach(item => {
-  item.style.color = 'blue';
-  console.log("item", item);
-});
-
-
-untilFound.addEventListener(
-  "beforematch",
-      () => (untilFound.textContent = "I've been revealed!"),
-  );
-  */
- 
 document.querySelector("#reset").addEventListener("click", () => {
   document.location.hash = "";
   document.location.reload();
