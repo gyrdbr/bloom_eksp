5fd763808e47a30e6454cc6c7a82bf855633f4f8
Gyrd Brændeland <gyrdbr@gmail.com>
2026-08-08 15:05:43 +0200

    Puh, Fatt tak i en inkscape-blomst som funker.

--- slider.js
+++ slider.js
@@ -30,7 +30,25 @@ prevBtn.addEventListener('click', () => {
 });
     
 // const allUntilFounds = document.querySelectorAll(".hiddenNow");
+/*
+const untilFound = document.querySelector("#until-found-box");
+const allFounds = document.querySelectorAll(".hiddenNow");
 
+console.log("allFounds", allFounds);
+
+// Loop through the items to apply changes
+allFounds.forEach(item => {
+  item.style.color = 'blue';
+  console.log("item", item);
+});
+
+
+untilFound.addEventListener(
+  "beforematch",
+      () => (untilFound.textContent = "I've been revealed!"),
+  );
+  */
+ 
 document.querySelector("#reset").addEventListener("click", () => {
   document.location.hash = "";
   document.location.reload();
