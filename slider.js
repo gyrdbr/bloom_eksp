debe036138a4ae96639d06f0fbf2290afd56f643
Gyrd Brændeland <gyrdbr@gmail.com>
2026-08-12 11:02:30 +0200

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,24 +1,16 @@
 const track = document.getElementById('sliderTrack');
 const track2 = document.getElementById('sliderTrack2');
-
 const slides = document.querySelectorAll('.slide');
 const slides2 = document.querySelectorAll('.slide2');
-
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-
 const prevBtn2 = document.getElementById('prevBtn2');
 const nextBtn2 = document.getElementById('nextBtn2');
-
-
-
-let currentIndex = 0;
-let currentIndex2 = 0;
-
 const totalSlides = slides.length;
 const totalSlides2 = slides2.length;
 
-
+let currentIndex = 0;
+let currentIndex2 = 0;
 
 function updateSlider() {
   // Shift the track left by the width of the active slide index
@@ -66,107 +58,3 @@ prevBtn2.addEventListener('click', () => {
   }
   updateSlider2();
 });
-
-
-
-
-
-
-/*
-document.querySelector("#resetHidden1").addEventListener("click", () => {
-  // document.location.hash = "";
-  // document.location.reload();
-  // console.leg("document.querySelector(resetHidden1)", document.querySelector("#resetHidden1"));
-  console.log("how many are shown")
-});
-*/
-
-/*
-const hiddenSections = [];
-
-function keepMaxOneArr(arr) {
-  // Shift the track left by the width of the active slide index
-
-  console.log("arr", arr);
-  if (arr.length < 1) {
-    hiddenSections.push("denne");
-
-    console.log("hallo?", hiddenSections);
-  } else {
-    document.location.reload();
-    console.log("keepMaxOneArr", arr, hiddenSections);
-    document.location.hash = "";
-    console.log("arr", arr, hiddenSections);
-    // document.location.reload();
-    // hiddenSections.remove();
-  }
-}
-
-document.querySelector("#resetHidden1").addEventListener("click", () => {
-  // document.location.hash = "";
-  // document.location.reload();
-  // hiddenSections.push("denne");
-  console.log("hiddenSections", "hiddenSections", hiddenSections, hiddenSections.length);
-
-  keepMaxOneArr(hiddenSections);
-});
-
-document.querySelector("#resetHidden2").addEventListener("click", () => {
-  // document.location.hash = "";
-  // document.location.reload();
-});
-*/
-
-const section1 = document.getElementById('until-found-box');
-const section2 = document.getElementById('until-found-box2');
-
-// console.log("untilFound.textContent");
-console.log("<section>", section1);
-console.log("<section class=hiddenNow rw-flower-page rw-section id=until-found-box hidden=until-found><div id=heiSveis class=hiddenHere>Hidden until found</div>");
-
-/* 
-<div id="heiSveis" class="hiddenHere">Hidden until found</div>
-*/
-
-
-const untilFound = document.querySelector("#until-found-box");
-const untilFound2 = document.querySelector("#until-found-box2");
-
-// hva er det som setter den til aa bli vist
-untilFound.addEventListener(
-  "beforematch",
-  () => (
-    untilFound.textContent = "I've been revealed 1!"
-  ),
-  console.log("untilFound", untilFound)
-);
-
-
-
-
-
-  // TODO: I moren
-// todo enten vises kun en uansett, ellers kan man ikke fjerne noen
-// her viser den skjulte
-// jeg tror man boer bruke denne men maa finne ut hvordan man kan bruke begge
-
-// console.log("untilFound", untilFound);
-
-
-untilFound2.addEventListener(
-  "beforematch2",
-  () => (untilFound2.textContent = "I've been revealed 2!"),
-);
-
-
-
-
-document.querySelector("#reset").addEventListener("click", () => {
-  document.location.hash = "";
-  document.location.reload();
-});
-
-
-// hidden
-
-// console.log("dist1", dist1);
